d5feeda576d765655dcbd353795c31f46e82ffae
Rocean <roceanadhikari@gmail.com>
2026-03-29 12:53:03 +0530

    changed drizzilke js

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
   schema: './lib/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',      // replaces 'driver: "pg"'
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,  // replaces 'connectionString'
   },
   verbose: true,
   strict: true,
-} satisfies Config;
+});
